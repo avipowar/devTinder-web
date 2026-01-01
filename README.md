5e1c1e7c9c3d72f9e86724f36130a90bb3a23643
Avinash Powar <powaravinash06@gmail.com>
2026-01-01 10:39:51 +0530

    set-up tailwind in my web-app

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
     - Create Vite web-Project
     - remove unecceray code
     - and initialize git to the project
+
+    - install tailwind in my project
+    - and setup the tailwind
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import React from "react";
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline"> Hello world!!</h1>
     </>
   );
 }
